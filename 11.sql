81243d2df4555cad7b8191e930eec9ed5fac0e2e
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-18 21:24:41 +0530

    Add query 11 to find patient with greatest height

---
+++ 11.sql
@@ -0,0 +1,9 @@
+/*
+  Description: Show the first_name, last_name, and height of the patient with the greatest height.
+*/
+SELECT 
+  first_name, 
+  last_name, 
+  height 
+FROM patients
+WHERE height = (SELECT MAX(height) FROM patients);
