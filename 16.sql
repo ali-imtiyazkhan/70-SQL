c3cd97ddbe4f5acbdeaee3cf01ba28fc4b23e347
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-19 16:30:11 +0530

    Add solutions for exercises 14, 15, and 16

--- 16.sql
+++ 16.sql
@@ -2,6 +2,7 @@
   Description: Based on the cities that our patients live in, show unique cities that are in province_id 'NS'.
   Note: Your answer will have 3 rows (Halifax, New Glasgow, Sydney).
 */
+
 SELECT DISTINCT city
 FROM patients
 WHERE province_id = 'NS';
