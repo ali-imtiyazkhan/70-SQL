863aa77e8c277742994637bb6d0c9d23a3e6f49c
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-19 17:14:19 +0530

    Add query to order patient first names by length and then alphabetically

---
+++ 23.sql
@@ -0,0 +1,6 @@
+/*
+  Description: Display every patient's first_name. Order the list by the length of each name and then alphabetically.
+*/
+SELECT first_name
+FROM patients
+ORDER BY LENGTH(first_name), first_name;
