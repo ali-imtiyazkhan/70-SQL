6830ca60d9577984bff1c3af08346518fa2d1d69
Imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-05-19 18:09:15 +0530

    Add query to display and sort patient counts by city

---
+++ 27.sql
@@ -0,0 +1,11 @@
+/*
+  Description: Show the city and the total number of patients in the city. Order from most to least patients and then by city name ascending.
+*/
+SELECT 
+    city, 
+    COUNT(*) AS total_patients
+FROM patients
+GROUP BY city
+ORDER BY 
+    total_patients DESC, 
+    city ASC;
